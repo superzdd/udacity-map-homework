f9f8e72a23d29801c553ecfd83ef151e83f96b6d
superzdd <superzdd@sina.com>
2017-12-29 20:15:37 +0800

    add ko

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var initBaiduMap = function() {
+	// 百度地图API功能
+	var map = new BMap.Map("allmap"); // 创建Map实例
+	map.centerAndZoom(new BMap.Point(116.404, 39.915), 11); // 初始化地图,设置中心点坐标和地图级别
+	//添加地图类型控件
+	map.addControl(new BMap.MapTypeControl({
+		mapTypes: [
+			BMAP_NORMAL_MAP,
+			BMAP_HYBRID_MAP
+		]
+	}));
+	map.setCurrentCity("上海"); // 设置地图显示的城市 此项是必须设置的
+	map.enableScrollWheelZoom(true); //开启鼠标滚轮缩放	
+	
+	return map;
+}
+
+var viewModel = function(){
+	this.map = ko.observable(initBaiduMap());
+}
+
+ko.applyBindings(new viewModel());
